2e3ab5ffc17da29700b605c23ee47d7c099cf6eb
ADRIELE1823 <136723178+ADRIELE1823@users.noreply.github.com>
2024-03-12 10:50:06 -0300

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PainelComponent } from './componentes/painel/painel.component';
+import { WelcomeComponent } from './componentes/welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+     {path: "painel", component: PainelComponent },
+    {path:"", component:WelcomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PainelComponent } from './painel/painel.component';
+import { PainelComponent } from './componentes/painel/painel.component';
+import { PrincipalComponent } from './componentes/principal/principal.component';
+import { WelcomeComponent } from './componentes/welcome/welcome.component';
+import { TurmasComponent } from './componentes/turmas/turmas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PainelComponent
+    PainelComponent,
+    PrincipalComponent,
+    WelcomeComponent,
+    TurmasComponent
   ],
   imports: [
     BrowserModule,
